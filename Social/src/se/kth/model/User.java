a52800c6b0b82ffb40d0f56f3c0261cbfdff8c75
Mats Maatson <mats.maatson@gmail.com>
2013-11-05 11:32:18 +0100

    JSF testing

--- Social/src/se/kth/model/User.java
+++ Social/src/se/kth/model/User.java
@@ -1,5 +1,39 @@
 package se.kth.model;
 
-public class User {
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class User implements Serializable  {
+
+	private static final long serialVersionUID = 1L;
+	
+	private int id;
+	private String username;
+	private String password;
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
 }
