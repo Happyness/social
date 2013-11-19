e784d1c50c4c0e454214d8141fad3043c2fc6c7d
Joel Denke <mail@happyness.se>
2013-11-19 15:47:44 +0100

    Latest fix

--- Social/src/se/kth/backend/SocialApplication.java
+++ Social/src/se/kth/backend/SocialApplication.java
@@ -6,11 +6,8 @@ import org.restlet.routing.Router;
 
 import se.kth.backend.resource.AuthServerResource;
 import se.kth.backend.resource.PrivateMessageServerResource;
-<<<<<<< HEAD
 import se.kth.backend.resource.ProfileServerResource;
-=======
 import se.kth.backend.resource.PrivateMessagesServerResource;
->>>>>>> b9cbaa5e445276c8ef3cf8881d02f5d198e9c1a2
 import se.kth.backend.resource.UserServerResource;
 import se.kth.backend.resource.UsersServerResource;
 import se.kth.backend.resource.WallServerResource;
@@ -25,7 +22,7 @@ public class SocialApplication extends Application
         router.attach("/user/{userid}", UserServerResource.class);
         router.attach("/user/store", UserServerResource.class);
         router.attach("/user/profile/{userid}", ProfileServerResource.class);
-        router.attach("/messages/{userid}", PrivateMessageServerResource.class);
+        router.attach("/messages/{userid}", PrivateMessagesServerResource.class);
         router.attach("/message/{userid}", PrivateMessageServerResource.class);
         router.attach("/wall/{userid}", WallServerResource.class);
         router.attach("/login", AuthServerResource.class);
