c76126892c0349ed1ec2ee199bc95bd8bb04cfc2
Mats Maatson <mats.maatson@gmail.com>
2013-11-20 16:55:35 +0100

    Fixed DOB

--- Social/src/se/kth/frontend/handler/WallHandler.java
+++ Social/src/se/kth/frontend/handler/WallHandler.java
@@ -25,86 +25,89 @@ import se.kth.frontend.handler.security.TokenSession;
 
 @ManagedBean
 @SessionScoped
-public class WallHandler implements Serializable
-{
+public class WallHandler implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	private String message;
 	private String response;
 	private int id = -1;
-	
-    @ManagedProperty(value = "#{tokenSession}")
-    private TokenSession tokenSession;
-    private UserProfile profile;
-    
-    public void load()
-    {
-		Map<String, String> map = (Map<String, String>) FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
+
+	@ManagedProperty(value = "#{tokenSession}")
+	private TokenSession tokenSession;
+	private UserProfile profile;
+
+	public void load() {
+		Map<String, String> map = (Map<String, String>) FacesContext
+				.getCurrentInstance().getExternalContext()
+				.getRequestParameterMap();
 		String id = map.get("id");
-		
+
 		if (id != null) {
 			this.id = Integer.parseInt(id);
 		} else {
-	    	UserProfile profile = tokenSession.getProfile();
-	    	if (profile != null) {
-	    		this.id = profile.getUserProfileId();
-	    	}
+			UserProfile profile = tokenSession.getProfile();
+			if (profile != null) {
+				this.id = profile.getUserProfileId();
+			}
 		}
-    }
-    
-    public void setTokenSession(TokenSession tokenSession)
-    {
-    	this.tokenSession = tokenSession;
-    }
-    
-    public TokenSession getTokenSession()
-    {
-    	return tokenSession;
-    }
-    
-    public boolean getMyOwn()
-    {
-    	UserProfile profile = tokenSession.getProfile();
-    	if (profile != null) {
-    		return (profile.getUserProfileId() == id);
-    	}
-    	return false;
-    }
-    
-    public List<UserLogMessage> getMessagesByUser()
-    {
-    	List<UserLogMessage> messages = new ArrayList<UserLogMessage>();
+	}
+
+	public void setTokenSession(TokenSession tokenSession) {
+		this.tokenSession = tokenSession;
+	}
+
+	public TokenSession getTokenSession() {
+		return tokenSession;
+	}
+
+	public boolean getMyOwn() {
+		UserProfile profile = tokenSession.getProfile();
+		if (profile != null) {
+			return (profile.getUserProfileId() == id);
+		}
+		return false;
+	}
+
+	public List<UserLogMessage> getMessagesByUser() {
+		List<UserLogMessage> messages = new ArrayList<UserLogMessage>();
 		System.out.println("DEBUG id: " + this.id);
-    	if (this.id > 0) {
-    		Representation wr = ClientHandler.getNormalResource("/wall/" + this.id);
-    		JsonRepresentation jsonRep;
+		if (this.id < 0) {
+			load();
+		}
+		if (this.id > 0) {
+			Representation wr = ClientHandler.getNormalResource("/wall/"
+					+ this.id);
+			JsonRepresentation jsonRep;
 			try {
 				jsonRep = new JsonRepresentation(wr);
-				messages = Converter.fromJsonToList(jsonRep.getText(), new TypeToken<List<UserLogMessage>>() {}.getType());
+				messages = Converter.fromJsonToList(jsonRep.getText(),
+						new TypeToken<List<UserLogMessage>>() {
+						}.getType());
 			} catch (IOException e1) {
 				e1.printStackTrace();
 			}
-			
+
 			ClientHandler.releaseResource(wr);
-    	}
-    	
-    	return messages;
-    }
-	
-	public void save()
-	{
-    	String jsonString = Converter.toJson(message);
-    	JsonRepresentation jsonRep = new JsonRepresentation(jsonString);
-    	System.out.println("Sending: " + jsonString);
-    	WallResource wr = ClientHandler.getObjectResource("/wall/" + getTokenSession().getProfile().getUserProfileId(), WallResource.class);
-		
-    	try {
+		}
+
+		return messages;
+	}
+
+	public void save() {
+		String jsonString = Converter.toJson(message);
+		JsonRepresentation jsonRep = new JsonRepresentation(jsonString);
+		System.out.println("Sending: " + jsonString);
+		WallResource wr = ClientHandler.getObjectResource("/wall/"
+				+ getTokenSession().getProfile().getUserProfileId(),
+				WallResource.class);
+
+		try {
 			setResponse(wr.postUserLogMessage(jsonRep).getText());
 		} catch (JSONException | IOException e) {
 			e.printStackTrace();
 		}
-    	
-    	ClientHandler.releaseResource(jsonRep);
+
+		ClientHandler.releaseResource(jsonRep);
 	}
 
 	public String getMessage() {
